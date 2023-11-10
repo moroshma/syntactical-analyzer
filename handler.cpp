17c7ee0c42f797a689b87f39da921539690fb564
“moroshma <artyomspvzkrik@gmail.com>
2023-11-11 02:31:38 +0300

    add TODO

--- handler.cpp
+++ handler.cpp
@@ -1,5 +1,27 @@
 //
 // Created by moroshma on 11.11.23.
 //
-
 #include "handler.h"
+
+//TODO Создать map с зарезервированными типами по типу char* : bool
+//TODO Создать map с новыми типами по типу char* : bool
+//TODO Создать map с переменными, которые были созданы
+
+
+
+
+CLASS, STRUCT, UNION;
+
+class classHandlerData {
+
+    // Имя, все переменные доступные на данный момент, хэш сумма конструктора.
+    //Добавить функцию для считывания данных из файлика
+
+    classHandlerData(int i, int b = 10) {
+
+    }
+
+    std::map[]
+
+
+};
