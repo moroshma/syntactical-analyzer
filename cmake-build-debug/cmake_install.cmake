dabb3b0f57a9397f71ed1bb773bae8998bf42ea8
moroshma_linux <artyomspvzkrik@gmail.com>
2023-12-11 01:43:20 +0300

    Добавлен обработчик ошибок, сделаны наработки по обработчику конструктора

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/moroshma/Documents/syntactical-analyzer
+# Install script for directory: /home/moroshma/syntactical-analyzer
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/moroshma/Documents/syntactical-analyzer/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/moroshma/syntactical-analyzer/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- handler.cpp
+++ handler.cpp
@@ -10,27 +10,34 @@
 // TODO Создать map с новыми типами по типу char* : bool
 // TODO Создать map с переменными, которые были созданы
 using namespace std;
-
 class dataClass {
 private:
+  enum error {
+    MALLOC_ERROR,
+    INVALID_BRACETS_VISION,
+    UNDEFIND_ERROR,
+    INVALID_DEFULT_INITIAL
+  };
   set<string> baseTypeSet;
   string newType;
   int inputBaseType();
+
+  void checkValid(string line);
+
+  bool check_in_class(string line);
+  bool check_out_class(string line);
+  void  print_error(string line, size_t current_line, error err);
   size_t checkConst(string line, string nameClass);
 
   size_t starLineType = 0;
   size_t endLineType = 0;
   size_t countConstructor = 0;
-
-
+  size_t current_line = 0;
 
 public:
   int findType(size_t start, string file);
-
   dataClass() { inputBaseType(); }
-
-  // Имя, все переменные доступные на данный момент, хэш сумма конструктора.
-  // Добавить функцию для считывания данных из файла
+  string addSpaceInStr(string str);
 };
 
 int dataClass::inputBaseType() {
@@ -72,18 +79,16 @@ int dataClass::findType(size_t start, string file) {
       if (line[strlen(line) - 1] == '\n') {
         line[strlen(line) - 1] = '\0';
       }
-
       string s = line;
-
+      s = addSpaceInStr(s);
       string delimiterSpace = " ";
       string delimiterBracket = "{";
-
       size_t pos = 0;
       string token;
 
-      while (!newType.length() && ((pos = s.find(delimiterSpace)) != string::npos ||
-             (pos = s.find(delimiterBracket)) != string::npos)) {
-
+      while (!newType.length() &&
+             ((pos = s.find(delimiterSpace)) != string::npos ||
+              (pos = s.find(delimiterBracket)) != string::npos)) {
         token = s.substr(0, pos);
         if (!flag && "class" == token) {
           flag = true;
@@ -93,18 +98,19 @@ int dataClass::findType(size_t start, string file) {
           flag = false;
           cout << token << endl;
         }
-        s.erase(0, pos + delimiterSpace.length() );
+        s.erase(0, pos + delimiterSpace.length());
       }
-        if (newType.length()) {
-            size_t countConst = checkConst(line, this -> newType);
-            cout << countConst << endl;
 
+      if (newType.length()) {
+        size_t countConst = checkConst(line, this->newType);
+        if (countConst) {
+            checkValid(line);
         }
+      }
+      this->current_line++;
     }
-
-
-
     free(line);
+
   } else {
     ret = OPEN_FILE_ERROR;
   }
@@ -112,34 +118,79 @@ int dataClass::findType(size_t start, string file) {
 }
 
 size_t dataClass::checkConst(string line, string nameClass) {
-    string s = line;
+  string s = line;
+
+  string delimiterSpace = " ";
+  string delimiterBracket = "\n";
+  string delimiterCBracket = "(";
+
+  size_t pos = 0;
+  string token;
+  size_t countConst = 0;
+
+  while ((pos = s.find(delimiterSpace)) != string::npos ||
+
+         (pos = s.find(delimiterBracket)) != string::npos ||
+         (pos = s.find(delimiterCBracket)) != string::npos) {
+
+    token = s.substr(0, pos);
+    if (nameClass == token) {
+      countConst++;
+    }
+    s.erase(0, pos + delimiterSpace.length());
+  }
+  return countConst;
+}
+
+string dataClass::addSpaceInStr(string str) {
+  string newSt = "";
+  for (size_t i = 0; i < str.length(); i++) {
+    if ('\n' == str[i] || '(' == str[i] || ')' == str[i] || '{' == str[i] ||
+        '}' == str[i]) {
+      newSt += " ";
+      newSt += str[i];
+      newSt += " ";
+    } else {
+      newSt += str[i];
+    }
+  }
+  return newSt;
+}
+
+void dataClass::checkValid(string line) {
+
+    size_t findet_open_bracket = line.find("(");
+    size_t findet_vision_bracket = line.find("::");
+
+    if (string::npos == findet_open_bracket && string::npos == findet_vision_bracket) {
+        print_error(line, this ->current_line, INVALID_BRACETS_VISION);
+    } else if (current_line > endLineType) {
+        check_out_class(line);
+    } else {
+        check_in_class(line);
+    }
+  }
 
 
+bool dataClass::check_in_class(string line) { return false; }
 
+bool dataClass::check_out_class(string line) { return false; }
 
-    string delimiterSpace = " ";
-    string delimiterBracket = "\n";
-    string delimiterCBracket = "(";
-
-    size_t pos = 0;
-    string token;
-    size_t countConst = 0;
-
-    while ((pos = s.find(delimiterSpace)) != string::npos ||
-
-                                 (pos = s.find(delimiterBracket)) != string::npos ||
-                                 (pos = s.find(delimiterCBracket)) != string::npos   ) {
-
-        token = s.substr(0, pos);
-        if (nameClass == token) {
-            countConst++;
+void dataClass::print_error(string line, size_t current_line, dataClass::error err) {
+    switch (err) {
+        case INVALID_BRACETS_VISION: {
+            cout << "In line: " << current_line + 1 << "error in quotes or scope operator line: " << line << endl;
+            break;
         }
+        case MALLOC_ERROR:
+            cout << "Memory error ＼(￣▽￣)／" << endl;
+            break;
+        case UNDEFIND_ERROR:
+            cout << "Undefined  error (╯°益°)╯彡┻━┻ LINE: "<<current_line + 1 <<  "string: " << line << endl;
+            break;
 
-
-        s.erase(0, pos + delimiterSpace.length() );
     }
 
-    return countConst;
 }
 
 int main() {
